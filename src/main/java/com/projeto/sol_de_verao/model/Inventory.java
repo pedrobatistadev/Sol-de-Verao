f7d401f0cf4689ea48351d36d1614df2217c7e85
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-06-28 20:39:00 -0300

    Model
    Entidade finalizada
    - Inventory

---
+++ src/main/java/com/projeto/sol_de_verao/model/Inventory.java
@@ -0,0 +1,54 @@
+package com.projeto.sol_de_verao.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "inventories")
+public class Inventory implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    public Inventory() {
+    }
+
+    public Inventory(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Inventory that)) return false;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
