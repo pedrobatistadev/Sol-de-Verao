49692d2c7c90c544d366be4bc27088b934f9c03f
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-07-02 23:26:31 -0300

    Controller
    Classe controladora finalizada
    - CategoryController

--- src/main/java/com/projeto/sol_de_verao/controllers/CategoryController.java
+++ src/main/java/com/projeto/sol_de_verao/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.projeto.sol_de_verao.controllers;
 
+import com.projeto.sol_de_verao.controllers.docs.CategoryControllerDocs;
 import com.projeto.sol_de_verao.dto.CategoryDTO;
 import com.projeto.sol_de_verao.dto.createDTO.CategoryCreateDTO;
 import com.projeto.sol_de_verao.services.CategoryService;
@@ -7,22 +8,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping(value = "/v1/category")
-public class CategoryController {
+public class CategoryController implements CategoryControllerDocs {
 
     @Autowired
     private CategoryService service;
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Override
     public ResponseEntity<CategoryDTO> create(@RequestBody CategoryCreateDTO category) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(category));
     }
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Override
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+
+    @PutMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryCreateDTO categoryCreateDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(id, categoryCreateDTO));
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete (@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
