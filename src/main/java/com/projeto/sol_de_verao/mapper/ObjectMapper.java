f0fbea2caa6edd098213dcbf673137af483e3079
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-07-02 23:27:34 -0300

    Config
    ObjectMapper finalizado
    - Conversão de Listas

--- src/main/java/com/projeto/sol_de_verao/mapper/ObjectMapper.java
+++ src/main/java/com/projeto/sol_de_verao/mapper/ObjectMapper.java
@@ -3,11 +3,23 @@ package com.projeto.sol_de_verao.mapper;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ObjectMapper {
 
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
     public static <O,D> D parseObject(O origin, Class<D> destiny) {
         return mapper.map(origin, destiny);
+    }
+
+    public static <O,D> List<D> parseList(List<O> origin, Class<D> destiny) {
+        List<D> destinyDTO = new ArrayList<>();
+
+        for (O data : origin) {
+            destinyDTO.add(parseObject(data, destiny));
+        }
+        return destinyDTO;
     }
 }
