32ace7b5985587c6665fe9f633806fb71ec2fec4
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-07-27 01:19:31 -0300

    Repository
    Implementada Query findByName

--- src/main/java/com/projeto/sol_de_verao/repository/CategoryRepository.java
+++ src/main/java/com/projeto/sol_de_verao/repository/CategoryRepository.java
@@ -2,6 +2,13 @@ package com.projeto.sol_de_verao.repository;
 
 import com.projeto.sol_de_verao.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT C FROM Category AS C WHERE C.description LIKE LOWER(CONCAT('%', :name, '%'))")
+    Optional<Category> findByName(@Param("name") String name);
 }
--- src/main/java/com/projeto/sol_de_verao/repository/InventoryRepository.java
+++ src/main/java/com/projeto/sol_de_verao/repository/InventoryRepository.java
@@ -1,7 +1,15 @@
 package com.projeto.sol_de_verao.repository;
 
+import com.projeto.sol_de_verao.model.Category;
 import com.projeto.sol_de_verao.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    @Query("SELECT I FROM Inventory AS I WHERE I.description LIKE LOWER(CONCAT('%', :name, '%'))")
+    Optional<Inventory> findByName(@Param("name") String name);
 }
