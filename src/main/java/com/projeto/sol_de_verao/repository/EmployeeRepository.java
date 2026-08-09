07b5df0c0030b565979162e5eb98ce9edd745305
Pedro Lucas Alves Batista <pedrolucasbatista26@gmail.com>
2026-08-09 13:11:41 -0300

    Repository
    Implementado método + query e limpeza de cache
    - Disable

--- src/main/java/com/projeto/sol_de_verao/repository/EmployeeRepository.java
+++ src/main/java/com/projeto/sol_de_verao/repository/EmployeeRepository.java
@@ -2,6 +2,13 @@ package com.projeto.sol_de_verao.repository;
 
 import com.projeto.sol_de_verao.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Employee AS e SET e.enabled = false WHERE e.id = :id")
+    void disable(@Param("id") Long id);
 }
--- src/main/java/com/projeto/sol_de_verao/repository/ProductRepository.java
+++ src/main/java/com/projeto/sol_de_verao/repository/ProductRepository.java
@@ -2,6 +2,13 @@ package com.projeto.sol_de_verao.repository;
 
 import com.projeto.sol_de_verao.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Product AS p SET p.enabled = false WHERE p.id = :id")
+    void disable(@Param("id") Long id);
 }
